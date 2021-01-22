2cd276c451a50ef6f41a885d9f4cec7cef9d67ec
Marlowe <meow990099@gmail.com>
2021-01-22 18:56:19 +0000

    Removed some spaces

---
+++ valid.php
@@ -0,0 +1,12 @@
+<?php 
+$allow = array("admin":"admin");
+if (in_array( $deny)) {
+   header("location: https://web-team-1.webteamone.repl.co/");
+   exit();
+} 
+else {
+   header("location: https://web-team-1.webteamone.repl.co/");
+   exit();
+} 
+
+?>
